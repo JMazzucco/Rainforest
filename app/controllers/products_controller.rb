79a7eb3ad666fd77fb95b4111c3d2768895995e6
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-21 15:58:14 -0400

    Writing product validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,28 +23,26 @@ class ProductsController < ApplicationController
   	else
   		render :new
   	end
-
   end
 
   def update
-  	@product = Product.find(Params[:id])
+    @product = Product.find(params[:id])
 
-  	if @product.update_attributes(product_params)
-  		redirect_to product_path(@product)
-  	else
-  		render :edit
-  	end
+    if @product.update_attributes(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
   end
 
   def destroy
-  	@product = Product.find(params[:id])
-  	@product.destroy
-  	redirect_to products_path
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
   private
-	def product_params
-		params.require(:product).permit(:name, :description, :price_in_cents)
-	end
-
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
 end
